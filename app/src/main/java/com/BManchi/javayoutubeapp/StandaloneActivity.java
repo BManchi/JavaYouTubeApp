fa1b4921da3f1120fc5baf36aac9d717a2a5ab55
Bruno <b.manchinelli@gmail.com>
2020-05-26 18:42:56 -0300

    fixes

--- app/src/main/java/com/BManchi/javayoutubeapp/StandaloneActivity.java
+++ app/src/main/java/com/BManchi/javayoutubeapp/StandaloneActivity.java
@@ -2,16 +2,13 @@ package com.BManchi.javayoutubeapp;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
-import com.google.android.youtube.player.YouTubeStandalonePlayer;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 
-/**
- * Created by timbuchalka on 22/07/2016.
- */
+
 
 public class StandaloneActivity extends AppCompatActivity implements View.OnClickListener {
     @Override
